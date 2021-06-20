eb930c961b565b4a47b5d093d01aa6c3e8e518a6
BokovRoman <r.bokov1991@gmail.com>
2021-06-20 22:45:49 +0300

    adds Routes to task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <Route path="/" exact component={HomePage} />
+    <Route path="/movies" component={MoviesPage} />
+    <Route path="/movies/:movieId" component={MovieDetailsPage} />
+    <Route path="/movies/:movieId/cast" component={Cast} />
+    <Route path="/movies/:movieId/reviews" component={Reviews} />
+  </>
+);
 
 export default App;
