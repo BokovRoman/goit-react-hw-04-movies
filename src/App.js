81dd2306ab15b40b2a63986d654b394038ede258
BokovRoman <r.bokov1991@gmail.com>
2021-06-21 00:03:52 +0300

    adds Switch to task

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import HomePage  from './views/HomePage';
 
 const App = () => (
   <>
+    <Switch>
     <Route path="/" exact component={HomePage} />
-    <Route path="/movies" component={MoviesPage} />
+    {/* <Route path="/movies" component={MoviesPage} />
     <Route path="/movies/:movieId" component={MovieDetailsPage} />
     <Route path="/movies/:movieId/cast" component={Cast} />
-    <Route path="/movies/:movieId/reviews" component={Reviews} />
+    <Route path="/movies/:movieId/reviews" component={Reviews} /> */}
+    </Switch> 
   </>
 );
 
